c6e1500fd205aabf11b4f2d86278fe06001e6579
novikov <novikov@drom.ru>
2020-07-12 15:08:58 +1000

    добавил пример использования доктрины

--- app/Entities/BadExample.php
+++ app/Entities/BadExample.php
@@ -2,6 +2,7 @@
 
 
 namespace App\Entities;
+
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -14,10 +15,41 @@ class BadExample
      * @ORM\id()
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue()
+     *
+     * @var int это для idea, не для доктрины!
      */
     protected $id;
+
+
     /**
-     *  @ORM\Column(type="string")
+     * @ORM\Column(type="string")
+     *
+     * @var string
      */
     protected $badExample;
+
+    /**
+     * @param $id
+     * @param $badExample
+     */
+    public function __construct(string $badExample)
+    {
+        $this->badExample = $badExample;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBadExample(): string
+    {
+        return $this->badExample;
+    }
 }
