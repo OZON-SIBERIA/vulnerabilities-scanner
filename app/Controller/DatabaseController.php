72ce38d355f1a8c9f6a745c42c9cb1061f001f0c
novikov <novikov@drom.ru>
2020-07-12 15:08:58 +1000

    добавил пример использования доктрины

--- app/Controller/DatabaseController.php
+++ app/Controller/DatabaseController.php
@@ -4,15 +4,31 @@
 namespace App\Controller;
 
 
-use App\Service\TwigInitiator;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use App\Config;
+use App\Entities\BadExample;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DatabaseController
 {
     public static function getReferencesList()
     {
-        return Response::create(TwigInitiator::twig()->render('spravka.html.twig'));
+        $em = Config::entityManager();
+        $badexample = new BadExample("Example created at" . date('Y-m-d H:i:s'));
+
+        //save
+        $em->persist($badexample);
+        $em->flush();
+
+        //get all
+        $badExampleRepository = $em->getRepository(BadExample::class);
+        return new JsonResponse(array_map(
+            function (BadExample $badExample) {
+                return ['id' => $badExample->getId(), 'example' => $badExample->getBadExample()];
+            },
+            $badExampleRepository->findAll()
+        ));
+
+        //return Response::create(TwigInitiator::twig()->render('spravka.html.twig'));
     }
 
     public static function getReference()
